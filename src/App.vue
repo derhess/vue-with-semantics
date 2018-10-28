978498c2dfdf6cce9d5629a52e11e1a5cadc5365
derhess <florianweil@gmx.de>
2018-10-28 14:55:08 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <with-microformats testProp="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WithMicroformats from "./components/WithMicroformats.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    WithMicroformats
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/WithMicroformats.vue
@@ -0,0 +1,22 @@
+<template>
+   <p class="h-geo">
+        <span class="p-latitude">-27.116667</span>
+        <span class="p-longitude">-109.366667</span>
+    </p>
+</template>
+
+<script>
+export default {
+  name: "testProp",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
